96cab02df5618ee64b4c09da80efaeb7ffb64d82
guilherme-fcm <guilherme_fcm@hotmail.com>
2022-10-17 21:38:43 -0300

    Add constructor methods on Category

--- src/main/java/org/api/model/Category.java
+++ src/main/java/org/api/model/Category.java
@@ -10,4 +10,13 @@ import javax.persistence.Id;
 public class Category extends PanacheEntity {
     public String description;
     public String initials;
+
+    public Category() {
+    }
+
+    public Category(Long id, String description, String initials) {
+        this.id = id;
+        this.description = description;
+        this.initials = initials;
+    }
 }
