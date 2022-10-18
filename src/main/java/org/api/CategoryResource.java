d584395c6f062443173052d5b6a784473b76ffbb
guilherme-fcm <guilherme_fcm@hotmail.com>
2022-10-17 22:11:50 -0300

    Set application_json on resource

--- src/main/java/org/api/CategoryResource.java
+++ src/main/java/org/api/CategoryResource.java
@@ -7,25 +7,30 @@ import javax.ws.rs.core.MediaType;
 import java.util.List;
 
 @Path("/category")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class CategoryResource {
 
     @GET
-    @Produces(MediaType.APPLICATION_JSON)
     public List<Category> findAll() {
         return Category.listAll();
     }
 
     @GET
     @Path("/{id}")
-    @Produces(MediaType.APPLICATION_JSON)
     public Category findById(@PathParam("id") Long id) {
         return Category.findById(id);
     }
 
     @DELETE
     @Path("/{id}")
-    @Produces(MediaType.APPLICATION_JSON)
     public void destroy(@PathParam("id") Long id) {
         Category.deleteById(id);
+    }
+
+    @POST
+    public Category create(Category category) {
+        Category.persist(category);
+        return category;
     }
 }
