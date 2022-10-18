a4c5a04d2fd4e847294d660d79eec18479919357
guilherme-fcm <guilherme_fcm@hotmail.com>
2022-10-17 21:40:58 -0300

    Use Path on method findById

--- src/main/java/org/api/CategoryResource.java
+++ src/main/java/org/api/CategoryResource.java
@@ -4,6 +4,7 @@ import org.api.model.Category;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
@@ -15,16 +16,17 @@ public class CategoryResource {
     @Produces(MediaType.APPLICATION_JSON)
     public List<Category> findAll() {
         return List.of(
-            new Category(1L, "Electronics", "ELT"),
-            new Category(2L, "Smartphones", "STP")
+                new Category(1L, "Electronics", "ELT"),
+                new Category(2L, "Smartphones", "STP")
         );
     }
 
     @GET
+    @Path("/{id}")
     @Produces(MediaType.APPLICATION_JSON)
-    public Category findById() {
+    public Category findById(@PathParam("id") Long id) {
         Category category = new Category();
-        category.id = 1L;
+        category.id = id;
         category.description = "Electronics";
         category.initials = "ELT";
         return category;
