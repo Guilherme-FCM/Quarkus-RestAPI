1e33da212257a30498db6536183e22213468bf13
guilherme-fcm <guilherme_fcm@hotmail.com>
2022-10-31 22:03:25 -0300

    Add by-directional relation

--- src/main/java/org/api/model/Category.java
+++ src/main/java/org/api/model/Category.java
@@ -1,11 +1,21 @@
 package org.api.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Category extends AbstractEntity {
     public String description;
     public String initials;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "category")
+    public List<Product> products = new ArrayList<>();
+
     public Category() {
     }
 
