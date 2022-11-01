4e5522ec488b86250309a9158e2716af01c1093e
guilherme-fcm <guilherme_fcm@hotmail.com>
2022-10-31 21:37:46 -0300

    Unexposed count end-point

--- src/main/java/org/api/resource/ProductResource.java
+++ src/main/java/org/api/resource/ProductResource.java
@@ -1,7 +1,10 @@
 package org.api.resource;
 
 import io.quarkus.hibernate.orm.rest.data.panache.PanacheEntityResource;
+import io.quarkus.rest.data.panache.MethodProperties;
 import org.api.model.Product;
 
 public interface ProductResource extends PanacheEntityResource<Product, Long> {
+    @MethodProperties(exposed = false)
+    long count();
 }
