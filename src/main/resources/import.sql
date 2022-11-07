523de482aed897bcf287c5cc2d37e07d6e9d6f35
Guilherme-FCM <guilherme_fcm@hotmail.com>
2022-11-07 20:44:32 -0300

    Insert products

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -3,7 +3,12 @@
 -- insert into myentity (id, field) values(nextval('hibernate_sequence'), 'field-1');
 -- insert into myentity (id, field) values(nextval('hibernate_sequence'), 'field-2');
 -- insert into myentity (id, field) values(nextval('hibernate_sequence'), 'field-3');
-insert into Category (description, initials) values
-                        ('Electronics', 'ELTC'),
-                        ('Smartphones', 'SMTP'),
-                        ('Domestic', 'DMTC');
+insert into Category (description, initials, version) values
+                        ('Electronics', 'ELTC', 0),
+                        ('Smartphones', 'SMTP', 0),
+                        ('Domestic', 'DMTC', 0);
+
+insert into Product (name, category_id, version) values
+                        ('Samsung Galaxy S20 FE', 1, 0),
+                        ('Samsung Galaxy S22 Ultra', 1, 0),
+                        ('Television', 2, 0);
